f5c880efdde7522e9d437672f846e17e0351be78
adityaraze <adityamodanwal293@gmail.com>
2024-07-02 15:36:21 +0530

    i change the package json file

--- server2/controllers/Auth.js
+++ server2/controllers/Auth.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken");
 const Profile = require("../models/Profile")
 const otpGenerator = require("otp-generator");
