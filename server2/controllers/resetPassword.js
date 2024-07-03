aae52f741bd43a6e350f15f76854e4cf9c16bbb4
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2024-07-03 09:01:10 +0530

    Update resetPassword.js

--- server2/controllers/resetPassword.js
+++ server2/controllers/resetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `http://localhost:3000/update-password/${token}`;
+		const url = `https://study-notion-frontend-theta-liard.vercel.app/${token}`;
 
 		await mailSender(
 			email,
