cd6967e4712a8fe6788773adffb7cf9a1eedc21f
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2024-10-05 19:25:49 +0530

    Update VideoDetails.jsx

--- src/components/core/ViewCourse/VideoDetails.jsx
+++ src/components/core/ViewCourse/VideoDetails.jsx
@@ -1,4 +1,12 @@
-const VideoDetails = () => {
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { markLectureAsComplete } from "../../../services/operations/courseDetailsAPI";
+import { updateCompletedLectures } from "../../../slices/viewCourseSlice";
+import 'video-react/dist/video-react.css'
+import { BigPlayButton, Player } from "video-react"
+import IconBtn from "../../Common/IconBtn";
+export const VideoDetails = () => {
   const { courseId, sectionId, subSectionId } = useParams();
   const { token } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -24,6 +32,7 @@ const VideoDetails = () => {
             const filteredData = courseSectionData.filter(
                 (course) => course._id === sectionId
             )
+            console.log("filterData",filteredData);
 
             const filteredVideoData = filteredData?.[0].SubSection?.filter(
                 (data) => data._id === subSectionId
@@ -137,103 +146,78 @@ const VideoDetails = () => {
     setLoading(false)
 
   };
-return (
-    
-  
-  <div className='flex flex-col gap-5 text-white mt-4 ml-2 shadow-[8px_8px_rgba(270,255,255)'>
-    {
-      !videoData ? (
-        <img
+  return (<div className="flex flex-col gap-5 text-white">
+    {!videoData ? (
+      <img
         src={previewSource}
         alt="Preview"
         className="h-full w-full rounded-md object-cover"
       />
-                  )
-      : (
-          <Player
-              ref = {playerRef}
-              aspectRatio="16:9"
-              playsInline
-              onEnded={() => setVideoEnded(true)}
-              src={videoData?.videoUrl}
-              
-              
-               >
+    ) : (
+      <Player
+        ref={playerRef}
+        aspectRatio="16:9"
+        playsInline
+        onEnded={() => setVideoEnded(true)}
+        src={videoData?.videoUrl}
+      >
+        <BigPlayButton position="center" />
+        {/* Render When Video Ends */}
+        {videoEnded && (
+          <div
+            style={{
+              backgroundImage:
+                "linear-gradient(to top, rgb(0, 0, 0), rgba(0,0,0,0.7), rgba(0,0,0,0.5), rgba(0,0,0,0.1)",
+            }}
+            className="full absolute inset-0 z-[100] grid h-full place-content-center font-inter"
+          >
+            {!completedLectures.includes(subSectionId) && (
+              <IconBtn
+                disabled={loading}
+                onclick={() => handleLectureCompletion()}
+                text={!loading ? "Mark As Completed" : "Loading..."}
+                customClasses="text-xl max-w-max px-4 mx-auto"
+              />
+            )}
+            <IconBtn
+              disabled={loading}
+              onclick={() => {
+                if (playerRef?.current) {
+                  // set the current time of the video to 0
+                  playerRef?.current?.seek(0)
+                  setVideoEnded(false)
+                }
+              }}
+              text="Rewatch"
+              customClasses="text-xl max-w-max px-4 mx-auto mt-2"
+            />
+            <div className="mt-10 flex min-w-[250px] justify-center gap-x-4 text-xl">
+              {!isFirstVideo() && (
+                <button
+                  disabled={loading}
+                  onClick={goToPreviousVideo}
+                  className="blackButton"
+                >
+                  Prev
+                </button>
+              )}
+              {!isLastVideo() && (
+                <button
+                  disabled={loading}
+                  onClick={goToNextVideo}
+                  className="blackButton"
+                >
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+        )}
+      </Player>
+    )}
 
-           
-              <BigPlayButton position="center" />
+    <h1 className="mt-4 text-3xl font-semibold">{videoData?.title}</h1>
+    <p className="pt-2 pb-6">{videoData?.description}</p>
+  </div>);
+};
 
-              {
-                  videoEnded && (
-                      <div
-                      style={{
-                        backgroundImage:
-                          " lg:linear-gradient(to top, rgb(0, 0, 0), rgba(0,0,0,0.7), rgba(0,0,0,0.5), rgba(0,0,0,0.1) md:linear-gradient(to top, rgb(0, 0, 0), rgba(0,0,0,0.7), rgba(0,0,0,0.5), rgba(0,0,0,0.1)",
-                      }}
-                      className="full absolute  inset-0 z-[100] grid h-full place-content-center lg:font-inter"
-                      >
-                          {
-                              !completedLectures.includes(subSectionId) && (
-                                  <IconBnt
-                                      disabled={loading}
-                                      onclick={() => handleLectureCompletion()}
-                                      text={!loading ? "Mark As Completed" : "Loading..."}
-                                      customClasses= 'lg:text-xl md:text-sm text-[8px] lg:max-w-max md:max-w-max lg:px-4 mx-auto  '
-                                  />
-                              )
-                          }
-
-                          <IconBnt
-                              disabled={loading}
-                              onclick={() => {
-                                  if(playerRef?.current) {
-                                    // set the current time of the video to 0
-                                      playerRef.current?.seek(0);
-                                      setVideoEnded(false);
-                                  }
-                              }}
-                              text="Rewatch"
-                              customClasses="lg:text-xl md:text-sm text-[8px] lg:max-w-max md:max-w-max px-4 mx-auto mt-2"
-                             
-                          />
-
-                          <div className='lg:mt-10  md:mt-10 mt-3 flex min-w-[250px] justify-center gap-x-4 lg:text-xl md:text-xl text-[8px]'>
-                              {!isFirstVideo() && (
-                                  <button
-                                  disabled={loading}
-                                  onClick={goToPrevVideo}
-                                  className='blackButton lg:text-xl md:text-sm text-[5px] lg:max-w-max md:max-w-max lg:p-4 md:p-4 p-2'
-                                  >
-                                      Prev
-                                  </button>
-                              )}
-                              {!isLastVideo() && (
-
-                                
-
-                                  <button
-                                  disabled={loading}
-                                  onClick={goToNextVideo}
-                                  className='blackButton lg:text-xl md:text-sm text-[5px] lg:max-w-max md:max-w-max lg:p-4 md:p-4 p-2   '>
-                                      Next
-                                  </button>
-                            
-                              )}
-                          </div>
-                      </div>
-                  )
-              }
-          </Player>
-      )
-    }
-    <h1 className='mt-4 lg:text-3xl md:text-2xl text-xl font-semibold'>
-      {videoData?.title}
-    </h1>
-    <p className='pt-2 pb-6'>
-      {videoData?.description}
-    </p>
-
- 
-  </div>
-)
-}
