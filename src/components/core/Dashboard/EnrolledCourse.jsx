99ff60eadccbcb252cdecda1a8d7daf7fdc178ed
Aditya Modanwal <135428914+adityaraze@users.noreply.github.com>
2024-10-06 09:57:20 +0530

    Update EnrolledCourse.jsx

--- src/components/core/Dashboard/EnrolledCourse.jsx
+++ src/components/core/Dashboard/EnrolledCourse.jsx
@@ -78,7 +78,7 @@ console.log("enrolledCourses = ",enrolledCourses);
                 className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
                 onClick={() => {
                   navigate(
-                    `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.SubSection?.[0]?._id}`
+                    `/view-course/${course?._id}/section/${course?.courseContent?.[0]?._id}/sub-section/${course?.courseContent?.[0]?.SubSection?.[0]?._id}`
                   )
                 }}
               >
